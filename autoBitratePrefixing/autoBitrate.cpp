37505b476f49ab1cff28f0b058078b3f97ae07e1
Shashank J <shashankreddy231@gmail.com>
2019-01-29 08:24:29 -0800

    Auto SI prefix.
    Auto select of SI prefixes for different bitrates based on the maximum size of output file.

---
+++ autoBitratePrefixing/autoBitrate.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+std::string exec(const char* cmd)
+{
+    std::array<char, 128> buffer;
+    std::string result;
+    auto pipe = popen(cmd, "r");
+    
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    
+    while (!feof(pipe))
+    {
+        if (fgets(buffer.data(), 128, pipe) != nullptr)
+            result += buffer.data();
+    }
+    
+    auto rc = pclose(pipe);
+    
+    if (rc == EXIT_SUCCESS)
+    {
+        std::cout << "SUCCESS\n";
+    }
+    else
+    {
+        std::cout << "FAILED\n";
+    }
+    
+    return result;
+}
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout<<"Start\n";
+    std::string res;
+    
+    res = exec("ffmpeg -i ../drop.avi -fs 900K outputMyMin.mp4");
+
+    res = exec("ffmpeg -i ../drop.avi -fs 10MB outputMyMax.mp4");
+
+    std::cout << "OUTPUT" << res << std::endl;
+    cout<<"End\n";
+    return 0;
+}
